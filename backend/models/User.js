b13d383be3972bc3751def5e6de8ee8c603f5f57
chaima-naweni <chaima.naoueni14@gmail.com>
2022-03-24 18:26:45 +0100

    registerLoginAuth

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,15 +1,29 @@
-const mongoose=require('mongoose');
-const usersSchema = mongoose.Schema({
+const mongoose = require('mongoose');
 
-    Name : {type:String, required:true},
-    ville : {type:String, required:true},
-    email : {type:String, required:true},
-    cin : {type:String},
-    password :{type:String, required:true},
-    tel :{type:Number, required:true},
-    role:{type:String, required:true},
+//------------ User Schema ------------//
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  verified: {
+    type: Boolean,
+    default: false
+  },
+  resetLink: {
+    type: String,
+    default: ''
+  }
+}, { timestamps: true });
 
-});
-module.exports =mongoose.model('User',usersSchema);
+const User = mongoose.model('User', UserSchema);
 
-
+module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -1,52 +1,56 @@
-const http = require('http');
-const app = require('./app');
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+require("./config/key");
+const app = express();
 
-// set the application port
-const normalizePort = val => {
-  const port = parseInt(val, 10);
+//------------ Passport Configuration ------------//
+require('./config/passport')(passport);
 
-  if (isNaN(port)) {
-    return val;
-  }
-  if (port >= 0) {
-    return port;
-  }
-  return false;
-};
-const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+//------------ DB Configuration ------------//
+const db = require('./config/key').DB;
 
-// Define an error handler
-const errorHandler = error => {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges.');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use.');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-};
+//------------ Mongo Connection ------------//
 
-// create a server
-const server = http.createServer(app);
 
-// set the error handler and a event listener
-server.on('error', errorHandler);
-server.on('listening', () => {
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-  console.log('Listening on ' + bind);
+//------------ EJS Configuration ------------//
+app.use(expressLayouts);
+app.use("/assets", express.static('./assets'));
+app.set('view engine', 'ejs');
+
+//------------ Bodyparser Configuration ------------//
+app.use(express.urlencoded({ extended: false }))
+
+//------------ Express session Configuration ------------//
+app.use(
+    session({
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true
+    })
+);
+
+//------------ Passport Middlewares ------------//
+app.use(passport.initialize());
+app.use(passport.session());
+
+//------------ Connecting flash ------------//
+app.use(flash());
+
+//------------ Global variables ------------//
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
 });
+//------------ Routes ------------//
+app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
 
-// make server listen on port
-server.listen(port);
+const PORT = process.env.PORT || 3004;
+
+app.listen(PORT, console.log(`Server running on PORT ${PORT}`));
