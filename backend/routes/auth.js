bfa0c01fefd88f7cfef6f5e3917ec38c15a3c07f
chaima-naweni <chaima.naoueni14@gmail.com>
2022-04-16 01:07:00 +0100

    token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,48 +2,40 @@ const router = require("express").Router();
 const { User } = require("../models/User");
 const Token = require("../models/token");
 const crypto = require("crypto");
+const jwt = require('jsonwebtoken');
 const sendEmail = require("../utils/sendEmail");
 const bcrypt = require("bcrypt");
 const Joi = require("joi");
 
 router.post("/", async (req, res) => {
-	try {
-		const { error } = validate(req.body);
-		if (error)
-			return res.status(400).send({ message: error.details[0].message });
-
-		const user = await User.findOne({ email: req.body.email });
-		if (!user)
-			return res.status(401).send({ message: "Invalid Email or Password" });
-
-		const validPassword = await bcrypt.compare(
-			req.body.password,
-			user.password
-		);
-		if (!validPassword)
-			return res.status(401).send({ message: "Invalid Email or Password" });
-
-		if (!user.verified) {
-			let token = await Token.findOne({ userId: user._id });
-			if (!token) {
-				token = await new Token({
+		User.findOne({ email: req.body.email })
+			.then(user => {
+			  if (!user) {
+				return res.status(401).json({ error: 'User not found !' });
+			  }
+			  bcrypt.compare(req.body.password, user.password)
+				.then(valid => {
+				  if (!valid) {
+					return res.status(401).json({ error: 'Wrong password !' });
+				  }
+				  res.status(200).json({
 					userId: user._id,
-					token: crypto.randomBytes(32).toString("hex"),
-				}).save();
-				const url = `${process.env.BASE_URL}users/${user._id}/verify/${token.token}`;
-				await sendEmail(user.email, "Verify Email", url);
-			}
+					token: jwt.sign(
+					  { userId: user._id },
+					  'RANDOM_TOKEN_SECRET',
+					  { expiresIn: '24h' }
+					)
+				  });
+				})
+				.catch(error => res.status(500).json({ error }));
+			})
+			.catch(error => res.status(500).json({ error }));
+	  
+				
 
-			return res
-				.status(400)
-				.send({ message: "An Email sent to your account please verify" });
-		}
-
-		const token = user.generateAuthToken();
-		res.status(200).send({ data: token,message: "logged in successfully" });
-	} catch (error) {
-		res.status(500).send({ message: "Internal Server Error" });
-	}
+		
+		
+	
 });
 
 const validate = (data) => {
